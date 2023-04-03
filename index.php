4da5372a0024d7f2a98a08e28a22b582351ed794
sheaqx <sheimanelabbar@gmail.com>
2023-04-03 15:21:15 +0200

    better html and css

--- index.php
+++ index.php
@@ -16,28 +16,30 @@
             <?php
             include 'generator.php';
             ?>
-            <div>
+            <div class="lenght-container">
                 <label for="length">Password length</label>
                 <div class="range-container">
                     <input type="range" id="length" name="length" min="0" max="50" value="20"><br>
                     <span id="length-value">20</span>
                 </div>
             </div>
-            <div>
-                <label for="lowercase">a-z</label>
-                <input type="checkbox" id="lowercase" name="include-lowercase" checked>
-            </div>
-            <div>
-                <label for="uppercase">A-Z</label>
-                <input type="checkbox" id="uppercase" name="include-uppercase" checked>
-            </div>
-            <div>
-                <label for="numbers">0-9</label>
-                <input type="checkbox" id="numbers" name="include-numbers">
-            </div>
-            <div>
-                <label for="symbols">/*·$)(%</label>
-                <input type="checkbox" id="symbols" name="include-symbols">
+            <div class="options-input">
+                <div class="lowercase-option">
+                    <input type="checkbox" id="lowercase" name="include-lowercase" checked>
+                    <label for="lowercase">a-z</label>
+                </div>
+                <div class="uppercase-option">
+                    <input type="checkbox" id="uppercase" name="include-uppercase" checked>
+                    <label for="uppercase">A-Z</label>
+                </div>
+                <div class="numbers-option">
+                    <input type="checkbox" id="numbers" name="include-numbers">
+                    <label for="numbers">0-9</label>
+                </div>
+                <div class="symbols-option">
+                    <input type="checkbox" id="symbols" name="include-symbols">
+                    <label for="symbols">/*·$)(%</label>
+                </div>
             </div>
             <input type="submit" value="Generate password">
         </form>
@@ -48,6 +50,10 @@
 
         passwordLengthRange.addEventListener('input', (event) => {
             passwordLengthValue.textContent = event.target.value;
+        });
+        const button = document.getElementById('myButton');
+        button.addEventListener('click', function() {
+            button.checked = !button.checked;
         });
     </script>
 
